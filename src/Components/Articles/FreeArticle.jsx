010dbfda297a5b37e089b1c50d74233b227649cd
gent0807 <theloopholesnk@gmail.com>
2023-09-20 15:18:16 +0900

    ...

--- src/Components/Articles/FreeArticle.jsx
+++ src/Components/Articles/FreeArticle.jsx
@@ -240,12 +240,11 @@ const FreeArticle = () => {
                 <DeleteBtn LoginMaintain={loginMaintain} User={user.login_state} UserInfo={userInfo} UserInfoState={userInfo==null ? null : userInfo.loginState} UserInfoNickname={userInfo==null ? (user.login_state==="allok" ? user.nickname : null): userInfo.nickName} Writer={writer} onClick={deleteArticle}>삭제</DeleteBtn>
                 <Link to="/FreeBoard" style={{color:"black"}}>목록</Link>
             </EditAllBox>
-            <img src="http://localhost:8033/EightBitBackend/resources/Users/seopseop/file/image/image.png" style={{width:"70px", height:"70px", borderRadius:"26px"}}></img>
-            <form style={{display:loginMaintain == null  ? "none" : loginMaintain=="true" ? (userInfo==null ? "none" : (userInfo.loginState==="allok"? (userInfo.nickName==writer? "block" :"none" ): "none" )):
-            (user.login_state==="allok" ? (user.nickname==writer ? "block":"none" ):"none" )}} onSubmit={registerReply}>
-                <textarea placeholder='댓글 내용' onChange={replyChange} value={replyChangeValue}></textarea>
-                <input type="button" value="댓글 등록"/>  
-            </form>
+                <form style={{display:loginMaintain == null  ? "none" : loginMaintain=="true" ? (userInfo==null ? "none" : (userInfo.loginState==="allok"? "block": "none" )):
+                (user.login_state==="allok" ? "block" :"none" )}} onSubmit={registerReply}>
+                    <textarea placeholder='댓글 내용' onChange={replyChange} value={replyChangeValue}></textarea>
+                    <input type="button" value="댓글 등록"/>  
+                </form>
         </FreeArticleBox >
     );
 }   
@@ -263,6 +262,7 @@ const DeleteBtn = styled.div
     (props.User==="allok" ? (props.UserInfoNickname==props.Writer ? "block":"none" ):"none" )};
     cursor : pointer;
     margin: 0px 0px 0px 13px;
+    color: black;
 `
 
 const LikeBtn = styled.div
