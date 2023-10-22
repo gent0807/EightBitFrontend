bbb8a4e2a41c9cb5d79a7e2995a5486ff1ef67a3
LANTOBOY <khs641010@naver.com>
2023-10-22 17:53:55 +0900

    cc

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -5,7 +5,7 @@ import { isDark } from './Darkmode/Darkmode';
 import { useEffect, useState } from 'react';
 import { Provider } from 'react-redux';
 
-const ip = `http://59.14.217.233:8033/EightBitBackend`;
+const ip = `http://59.14.217.233:8035`;
 //'http://localhost:8035'
 
 const Main = () => {
